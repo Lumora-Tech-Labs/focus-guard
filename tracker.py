e3ec453399d8bd3669305220444fce0ad6940628
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-04-11 21:45:20 +0500

    feat: record title and time every 5 seconds until tracker stopped with keyboard interrupt

--- tracker.py
+++ tracker.py
@@ -1,16 +1,27 @@
 import pygetwindow as gw
+import time as t
 
 def main():
-    # Getting all windows that are currently running
-    all_windows = gw.getAllWindows()
-    
-    # Iterate over every window
-    for window in all_windows:
-        # Get the title for each window
-        title_window = window.title
-        # If the title exists it is printed
-        if title_window.strip():
-            print(f"Found window: {window.title}")
+    try: 
+        # Keep recording title and time until tracker stopped
+        while True:
+            # Getting all windows that are currently running
+            all_windows = gw.getAllWindows()
+            
+            # Getting the time of capture
+            current_time = t.ctime()
+            
+            # Iterate over every window
+            for window in all_windows:
+                # Get the title for each window
+                title_window = window.title
+                # If the title exists it is printed
+                if title_window.strip():
+                    print(f"Found window: {window.title}, at time: {current_time}")
+            # Pause for 5 seconds
+            t.sleep(5)
+    except KeyboardInterrupt:
+        print("Tracker Stopped.")
 
 
 if __name__ == "__main__":
