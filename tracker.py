e63daae9aec95f08aa54783a5d7b9b6bc13cdbe8
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-04-11 22:03:45 +0500

    feat: logging the recorded data into a file

--- tracker.py
+++ tracker.py
@@ -1,23 +1,34 @@
 import pygetwindow as gw
 import time as t
+import csv
 
 def main():
+    # Field names for csv file
+    field_names = ["Window", "Time"] 
+    # File path
+    file_path = "./data/raw/data.csv"
+    
     try: 
+        print("Tracker started... logging data to data.csv")
         # Keep recording title and time until tracker stopped
         while True:
             # Getting all windows that are currently running
             all_windows = gw.getAllWindows()
-            
             # Getting the time of capture
             current_time = t.ctime()
             
-            # Iterate over every window
-            for window in all_windows:
-                # Get the title for each window
-                title_window = window.title
-                # If the title exists it is printed
-                if title_window.strip():
-                    print(f"Found window: {window.title}, at time: {current_time}")
+            # Open file to append
+            with open(file_path, "a", newline="") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=field_names)
+                # Iterate over every window
+                for window in all_windows:
+                    # Get the title for each window
+                    title_window = window.title
+                    # If the title exists it is printed
+                    if title_window.strip():
+                        print(f"Found window: {window.title}, at time: {current_time}")
+                        # Log to file
+                        writer.writerow({"Window" : title_window, "Time" : current_time})
             # Pause for 5 seconds
             t.sleep(5)
     except KeyboardInterrupt:
